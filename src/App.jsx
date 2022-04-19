f574fb049802410895661b35bf55de01001c7606
Arthur Santana <arthur.alexandre330@gmail.com>
2022-04-19 12:08:23 -0300

    função de exclusão adicionada

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
-import './App.css';
-import Card from './components/Card/Card';
+import "./App.css";
+import Card from "./components/Card/Card";
+import React, { useState } from "react";
 
 function App() {
-  return (
-   <>
-	<Card cardTitle="" cardContent=""/>
-   </>
-  );
+	const [notes, setNotes] = useState([
+		{
+			title: "título 1",
+			content: "conteúdo 1",
+		},
+		{
+			title: "título 2",
+			content: "conteúdo 2",
+		},
+	]);
+
+	return (
+		<>
+			{notes.map(note => 
+				<Card 
+					cardTitle={note.title} 
+					cardContent={note.content} 
+					setNotes={setNotes.bind(this)} 
+					notes={notes}
+					key={notes.indexOf(note)} 
+					id={notes.indexOf(note)}
+				/>
+			)}
+			<button 
+				onClick={
+					() => { setNotes([...notes, 
+							{ 
+								title: "título 3", 
+								content: "conteúdo 3" }]) 
+							}
+					}>
+					adicionar card
+			</button>
+		</>
+	)
+
 }
 
 export default App;
